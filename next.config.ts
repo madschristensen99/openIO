86b47b475d3eec7367277650089b0445542fe12d
undefinedlab <thechakralabs@gmail.com>
2025-11-22 21:27:21 +0100

    Fix Vercel build: exclude 0g directory from TypeScript compilation

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Exclude 0g directory from build as it's a separate project
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  webpack: (config, { isServer }) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/0g/**', '**/node_modules/**'],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
