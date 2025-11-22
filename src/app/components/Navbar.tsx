7fdf290085c4531182402b3784ede9cdf10a37a9
undefinedlab <thechakralabs@gmail.com>
2025-11-22 16:17:59 +0100

    Add dapp components: CardScanner, DappTabs, DappMainContent, DappHeader, DappFeatures, DappContent, and ThreeBackground

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-content">
+        <Link href="/" className="navbar-logo">
+          <Image 
+            src="/io_logo2.svg" 
+            alt="openIO Logo" 
+            width={32} 
+            height={22}
+            className="logo-image"
+            priority
+            unoptimized
+          />
+        </Link>
+        <div className="navbar-links">
+          <Link href="/dapp/models" className="nav-link">Models</Link>
+          <Link href="/dapp/datasets" className="nav-link">Datasets</Link>
+          <Link href="/community" className="nav-link">Community</Link>
+          <Link href="/docs" className="nav-link">Docs</Link>
+          <Link href="/pricing" className="nav-link">Pricing</Link>
+          <Link href="/dapp/builder" className="nav-link">Builder</Link>
+          <Link href="/profile" className="nav-link">Profile</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
